c7c10cc2cd06e98745d3f3419c7c309e2361f209
Enrique <icodeat9>
2020-11-21 11:56:09 -0400

    making sure applicationwithoutjquery compiles

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( industry.css )
-Rails.application.config.assets.precompile += %w( application-without.jquery.js team_dashboard/main.js team_dashboard/viewport.js )
+Rails.application.config.assets.precompile += %w( application-without-jquery.js team_dashboard/main.js team_dashboard/viewport.js )
 Rails.application.config.assets.precompile += %w( team_dashboard/main.css )
 Rails.application.config.assets.precompile += %w( ckeditor/config.js ckeditor/styles.js ckeditor/contents.css )
 Rails.application.config.assets.precompile += %w( ckeditor/filebrowser/images/*  )
